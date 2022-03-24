c7740b9f3f05d2641b30a7d012f486648c699e48
Chris Brown <brownca@email.arizona.edu>
2022-03-23 19:23:11 -0500

    Better initial commit

---
+++ src/kernels.cu
@@ -0,0 +1,19 @@
+// version 0
+// global memory only version
+__global__ void global_GaB_Decoder_kernel() {
+    // insert your code here
+}
+
+
+// version 1
+// shared memory privatized version
+__global__ void shared_GaB_Decoder_Kernel() {
+    // insert your code here
+}
+
+
+// version 2
+// streamed version
+__global__ void streamed_GaB_Decoder_kernel() {
+    // insert your code here
+}
---
+++ src/sources.cmake
@@ -0,0 +1,2 @@
+add_lab("GaB")
+add_lab_solution("GaB" ${CMAKE_CURRENT_LIST_DIR}/GaB.cu)
